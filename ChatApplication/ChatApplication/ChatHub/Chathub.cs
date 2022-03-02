e1e53b5171444fd09cc677a1f60ee61950c87bae
Semaeva <wunata95@gmail.com>
2022-03-02 21:07:03 +0600

    Добавление SignalR Core
    Добавила SignalR , а также форму для предачи оналйн - сообщени

---
+++ ChatApplication/ChatApplication/ChatHub/Chathub.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.AspNetCore.SignalR;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ChatApplication.ChatHub
+{
+    public class Chathub : Hub
+    {
+        public async Task Send(string message, string userName)
+        {
+            await Clients.All.SendAsync("Send", message, userName);
+        }
+    }
+}
